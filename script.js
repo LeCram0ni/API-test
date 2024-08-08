58a849373c385aecd9fcf0afd49689b9e2eda424
marcel <15247414+LeCram0ni@users.noreply.github.com>
2024-08-08 23:38:50 +0200

    first commit, temps winds times in triple per async function

---
+++ script.js
@@ -0,0 +1,53 @@
+/*let urlName = "Fritz";
+let url = "https://api.agify.io/?name=" + urlName;
+
+fetch(url)
+  .then((res) => res.json())
+  .then((data) =>
+    console.log(data.name + " must be " + data.age + " years old.")
+  );
+*/
+
+let key = "1d14b7f6fe48130a8dda5e925658331a";
+let city = "Essen";
+let lat = 51.4698;
+let lon = 6.9517;
+let temps = [];
+let winds = [];
+let times = [];
+let triple = [];
+let res;
+let data;
+
+/*
+let url =
+  "http://api.openweathermap.org/geo/1.0/direct?q= + city + &limit=5&appid=" +
+  key;
+*/
+let url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,&hourly=wind_speed_10m&hourly=temperature_2m";
+
+async function fetchData() {
+  try {
+    res = await fetch(url);
+    data = await res.json();
+
+    temps = data.hourly.temperature_2m.slice(0, 24);
+    winds = data.hourly.wind_speed_10m.slice(0, 24);
+    times = data.hourly.time.slice(0, 24);
+
+    for (i = 0; i < 24; i++) {
+      triple.push([temps[i], winds[i], times[i]]);
+    }
+
+    return triple;
+
+  } catch (e) {
+    console.error("ERROR " + e);
+  }
+
+}
+
+fetchData().then(triple => {
+  console.log(triple);
+});
+
